eff2215eae64e08a6784845aea04bb90d84e7c53
Sentinels Programming <sentinels.pro@gmail.com>
2023-02-14 16:26:26 -0500

    Added new IMU methods

--- src/main/java/com/sentinels/robot/subsystems/odometry/IMU.java
+++ src/main/java/com/sentinels/robot/subsystems/odometry/IMU.java
@@ -9,6 +9,7 @@
 package com.sentinels.robot.subsystems.odometry;
 
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj.ADIS16448_IMU.IMUAxis;
 
 public class IMU {
 
@@ -18,5 +19,18 @@ public class IMU {
         if (imu.isConnected() != true) {
             // handle connection issue?
         }
+    }
+
+    public double getAccelX(){
+        return imu.getAccelX(); //right is positive to the imu
+    }
+    public double getAccelY(){
+        return imu.getAccelY(); //forward is positive 
+    }
+    public double getAccelZ(){
+        return imu.getAccelZ(); //up is positive
+    }
+    public double getPitch(){
+        return imu.getYFilteredAccelAngle();// will get the pitch of the robots angle with acceleration filtered out
     }
 }
