fb12407fee2b31a39772cdb7fafac00af3005f22
Henry Cao <henrycao0308@gmail.com>
2023-02-16 22:12:58 -0500

    made variables and peudocode for autonTurn

--- src/main/java/com/sentinels/robot/commands/autonomous/Driving/SeperateDrive/AutonTurn.java
+++ src/main/java/com/sentinels/robot/commands/autonomous/Driving/SeperateDrive/AutonTurn.java
@@ -8,6 +8,7 @@ import com.sentinels.robot.subsystems.drive.Drivetrain;
 import com.sentinels.robot.subsystems.vision.Limelight;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutonTurn extends CommandBase {
@@ -18,7 +19,7 @@ public class AutonTurn extends CommandBase {
   private final PIDController directionController;
   //NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
-  public AutonTurn(Drivetrain drivetrain, Limelight limelight) {
+  public AutonTurn(Drivetrain drivetrain, Limelight limelight, double xoffset) {
     this.drivetrain = drivetrain;
     this.limelight = limelight;
     directionController = new PIDController(0.1, 0.1, 0.1);
@@ -34,7 +35,17 @@ public class AutonTurn extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    NetworkTableEntry xoffset = limelight.getTx(); //gets the angle offset of the gamepiece
+    double radiusS = 3.0; //radius of the smaller wheel
+    double distanceT; //distance between driveTrain wheels
+/*
+ * peudocode:
+ * 
+ * we first get the circumference of the entire robot using the distance between the two drivetrains as a radius
+ * the xoffset/360 is proportional to distance_turn_to_get_degree / total_circumference
+ * and once we have the distance_turn_to_get_degree, amount_of_rotations_per_smaller_wheel x 2pi(radius_of_smaller_wheel) = distance_turn_to_get_degree
+ * so we could use that equation to solve for amount_of_rotations_per_smaller_wheel
+*/
   }
 
   // Called once the command ends or is interrupted.
