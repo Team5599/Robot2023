e3493d001ab3af0dcbe344f0f53c03f8709987ed
Sentinels - Programming <sentinels.pro@gmail.com>
2023-02-18 14:18:45 -0500

    Lower speed of motors by 20%

--- src/main/java/com/sentinels/robot/commands/drivetrain/DrivetrainDrive.java
+++ src/main/java/com/sentinels/robot/commands/drivetrain/DrivetrainDrive.java
@@ -18,7 +18,7 @@ public class DrivetrainDrive extends CommandBase {
 
   double driverLeftY;
   double driverRightY;
-  
+
   public DrivetrainDrive(Drivetrain drivetrain, CommandXboxController controller) {
     this.m_Drivetrain = drivetrain;
     this.driver = controller;
@@ -39,7 +39,7 @@ public class DrivetrainDrive extends CommandBase {
     driverRightY = driver.getRightY();
 
     // Make both input values negative due to Y-axis up being negative
-    m_Drivetrain.tankDrive(driverLeftY, driverRightY);
+    m_Drivetrain.tankDrive(0.8 * driverLeftY,0.8 * driverRightY);
   }
 
   // Called once the command ends or is interrupted.
