70060cb30b20c2716a907531f6cb371715c55e5a
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-02-20 12:53:54 -0500

    Update constants

--- src/main/java/com/sentinels/robot/constants/Settings.java
+++ src/main/java/com/sentinels/robot/constants/Settings.java
@@ -12,6 +12,13 @@ public interface Settings {
 
     public interface Drivetrain {
         double DRIVESPEEDCAP = 0.8;
+
+        double GEAR_RATIO = 8.45; // 8.45:1 Ratio
+        double DRIVEBASE_KG = 22.6796; // 50 lbs.
+        double WHEEL_DIAMETER = 6; // inches
+        double WHEEL_TRACK_WIDTH = 22.9; // inches
+        double CENTER_MOMENT_INERTIA = 5.1349; // kg/m^2
+        int GEARBOX_NUM_MOTORS = 2;
     }
 
 }
