8c1a1a1ab94601105408dcf8fca1d863e0f4ca93
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-03-19 11:06:18 -0400

    Fix PivotPID

--- src/main/java/com/sentinels/robot/commands/armmech/arm/pivotPID.java
+++ src/main/java/com/sentinels/robot/commands/armmech/arm/pivotPID.java
@@ -4,24 +4,41 @@
 
 package com.sentinels.robot.commands.armmech.arm;
 
-import com.sentinels.robot.subsystems.arm.*;
+import com.sentinels.robot.constants.Settings;
+
+import com.sentinels.robot.constants.Settings.Arm.level;
+import com.sentinels.robot.subsystems.arm.Arm;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class pivotPID extends CommandBase {
+public class PivotPID extends CommandBase {
   private final Arm arm;
   private final PIDController pivotController;
 
-
-
-  //TODO: we need a command to callibrate the arm pivot motor
-  public pivotPID(Arm arm, double setpoint) {
+  public PivotPID(Arm arm, double angle) {
     this.arm = arm;
-    pivotController = new PIDController(0, 0, 0);
-    pivotController.setSetpoint(setpoint * 27);
+    pivotController = new PIDController(0.5,0,0);
+    pivotController.setSetpoint(27*angle);//using 27 here because thats the gear ratio
+    //addRequirements(arm); // becuase this is used in a parallel command, using addRequirements may interfere with functionality
+  }
 
-    addRequirements(arm);
+  public PivotPID(Arm arm, level height) {
+    this.arm = arm;
+    pivotController = new PIDController(0.5,0,0);
+
+    double setpoint = 0;
+    switch(height){
+      case LOW:
+        setpoint = 0;
+      case MEDIUM:
+        setpoint = 35;
+      case TOP:
+        setpoint = 70;
+    }
+    pivotController.setSetpoint(setpoint * Settings.Arm.kPivotGearRatio);
+
+    //addRequirements(arm);
   }
 
   @Override
@@ -30,16 +47,25 @@ public class pivotPID extends CommandBase {
   }
 
   @Override
-  public void execute() {
+  public void execute() {   
+    //TODO: there needs to be a way to detect the start position and also a way to callibrate everything properly
+    // distance may have to be negated
+    // if the pivot starts at 0 degrees, then we need to choose the bottom position to be the actual max amount of degrees, but the input should still be normal
+    // input should be from 0 to `70 degrees but map to the proper places
+    //arm outer length: 47", arm inner length: 40", base length: 28.5"
     arm.PivotArm(pivotController.calculate(arm.getRightPosition()));
+
+    //SmartDashboard.putNumber("Arm/PID",pivotController.getPositionError());
   }
 
+  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(pivotController.atSetpoint()){
+    if(pivotController.atSetpoint() == true){
       return true;
     }
     return false;
